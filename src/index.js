ffb97011f110ce9bd12dc67773414601325c64be
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-29 08:21:35 +0300

    default files removed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Navbar from "./Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import 'bootstrap/dist/js/bootstrap.min.js';
@@ -8,7 +7,6 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 function App() {
     return(
         <>
-    <Navbar/>
     </>
     );
 }
