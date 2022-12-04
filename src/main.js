1f074e186a8e68bf6e6d9b42215cb7d03dffbd5b
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-04 17:48:10 +0300

    https request resolved for many cities

--- src/main.js
+++ src/main.js
@@ -1,85 +1,52 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect } from "react";
 
 const cityName = [
-  "Mumbai",
-  "Delhi",
-  "Bangalore",
-  "Hyderabad",
-  "Chennai",
-  "Kolkata",
-  "Pune",
-  "Ahmedabad",
-  "Jaipur",
-  "Surat",
-  "Lucknow",
-  "Kanpur",
-  "Nagpur",
-  "Patna",
-  "Indore",
-  "Thane",
-  "Bhopal",
-  "Visakhapatnam",
-  "Vadodara",
-  "Firozabad",
-  "Ludhiana",
-  "Rajkot",
-  "Agra",
-  "Siliguri",
-  "Nashik",
-  "Faridabad",
-  "Patiala",
-  "Meerut",
-  "Kalyan-Dombivali",
-  "Vasai-Virar",
-  "Varanasi",
-  "Srinagar",
-  "Dhanbad",
-  "Jodhpur",
-  "Amritsar",
-  "Raipur",
-  "Allahabad",
-  "Coimbatore",
-  "Jabalpur",
-  "Gwalior",
-  "Vijayawada",
-  "Madurai",
-  "Guwahati",
-  "Chandigarh",
-  "Hubli-Dharwad",
-  "Amroha",
-  "Moradabad",
-  "Gurgaon",
-  "Aligarh",
-  "Solapur",
-  "Ranchi",
-  "Jalandhar",
-  "Tiruchirappalli",
-  "Bhubaneswar",
-  "Salem",
-  "Warangal",
-  "Mira-Bhayandar",
-  "Thiruvananthapuram",
-  "Bhiwandi",
+  "London",
+  "New York",
+  "Tokyo",
+  "Paris",
+  "Berlin",
+  "Moscow",
 ];
 
+const apiCall = async ()=>{
+  let data = [];
+  let cities = [];
+  let promises = [];
+
+  for (let i = 0; i < cityName.length; i++) {
+    promises.push(
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName[i]}&appid=32e0ef17bef62761317bf1cbfa079e5b`).then(response => {
+        cities.push(response);
+      })
+    )
+  }
+  await Promise.all(promises).then(data = cities);
+  return data;
+  
+}
 export default function Main() {
-        const city = cityName[0];
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=32e0ef17bef62761317bf1cbfa079e5b`
-      )
-      .then((res) => {
-        console.log(res.data);
-      });
-     
+  const city = cityName[0];
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+  useEffect(() => {
+      async function apiCalled(){
+        const tempData = await apiCall();
+        setData(tempData);
+      }
+      setInterval(()=>apiCalled(),5000)
+    },[city]
+  )
 
   return (
     <>
       <div className="container">
         <div className="row mt-5">
           <div className="col-4 bg-primary border rounded">
-         {}
+           {data?.map(item => <div>{item.data.name}<h2>{item.data.main.temp}</h2></div>)}
           </div>
         </div>
       </div>
