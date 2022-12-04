c07497c4f5171215a8f3e03b175ba74c7acead77
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-04 14:22:18 +0300

    axios fetching for many cities

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React from "react";
 
-const baseURL = "https://api.openweathermap.org/data/2.5/weather?q=nairobi,kenya&appid=c60704567ca7e06c9044295363aae45f";
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?q=nairobi&appid=c60704567ca7e06c9044295363aae45f";
 
 export default function Main() {
   const [post, setPost] = React.useState(null);
@@ -15,8 +15,16 @@ export default function Main() {
   if (!post) return null;
 
   return (
-    <div>
-      <p>{post.name}</p>
+    <div className="container mt-5 border p-4 rounded">
+      <div className="row">
+        <div className="col-4 bg-warning">
+          <h6>{post.name}</h6>
+          <span className="d-block">  {post.weather[0].description}</span>
+          <span className="d-block"> Minimum Temprature: {post.main.temp_min}</span>
+          <span> Maximum Temprature:{post.main.temp_max}</span>
+      </div>
+      </div>
     </div>
   );
 }
+   
