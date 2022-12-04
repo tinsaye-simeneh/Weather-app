8409e3a7adfa60e9110fec90a4c59dc7c64895ab
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-04 13:35:10 +0300

    function changed to Main

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import 'bootstrap/dist/js/bootstrap.min.js';
-import Quotes from "./main";
+import Main from "./main";
 
 function App() {
     return(
         <>
-        <Quotes/>
+        <Main/>
     </>
     );
 }
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const baseURL = "https://api.openweathermap.org/data/2.5/weather?q=nairobi,kenya&appid=c60704567ca7e06c9044295363aae45f";
 
-export default function App() {
+export default function Main() {
   const [post, setPost] = React.useState(null);
 
   React.useEffect(() => {
