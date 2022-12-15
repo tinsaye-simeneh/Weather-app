c2437efb87dc4a12a734e85d2f49b936ee76ddd0
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-15 13:37:01 +0300

    navbar responsiveness final

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,25 +6,25 @@ import linkedin from "./assets/img/linkedin.svg";
 const Navbar = () => {
   return (
     <div className="Container-fluid bg-dark">
-      <div className="row ps-5">
-        <div className="col-md-8 ms-3 text-white pt-3 ps-5">Windy Weather App</div>
-        <div className="col-md-3 ms-5">
-          <div className="row ps-5">
-            <div className="col-md-3 text-center">
-              <img src={github} alt="github" className="img-fluid w-50 my-3" />
+      <div className="row">
+        <div className="col-md-8 col-12 text-white navbar-title">Windy Weather App</div>
+        <div className="col-md-3 col-12">
+          <div className="row right-icons">
+            <div className="col-md-3 col-3 text-center">
+              <img src={github} alt="github" className="img-fluid my-3" />
             </div>
-            <div className="col-md-3 text-center">
+            <div className="col-md-3 col-3 text-center">
               <img
                 src={behance}
                 alt="behance"
-                className="img-fluid w-50 my-3"
+                className="img-fluid my-3"
               />
             </div>
-            <div className="col-md-3">
+            <div className="col-md-3 col-3">
               <img
                 src={linkedin}
                 alt="linkedin"
-                className="img-fluid w-50 my-3"
+                className="img-fluid my-3"
               />
             </div>
           </div>
