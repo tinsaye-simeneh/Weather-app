fe6695954029a86994fc021b781f3b03c615afb5
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-15 21:30:42 +0300

    navbar icons linked to their respective links

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,21 +11,28 @@ const Navbar = () => {
         <div className="col-md-3 col-12">
           <div className="row right-icons">
             <div className="col-md-3 col-3 text-center">
+              <a href="https://github.com/tinsaye-simeneh/Weather-app" target="_blank" rel="noreferrer">
               <img src={github} alt="github" className="img-fluid my-3" />
+              </a>
             </div>
+
             <div className="col-md-3 col-3 text-center">
+              <a href="https://www.behance.net/gallery/159186683/Simple-Weather-App" target="_blank" rel="noreferrer">
               <img
                 src={behance}
                 alt="behance"
                 className="img-fluid my-3"
               />
+              </a>
             </div>
             <div className="col-md-3 col-3">
+              <a href="https://www.linkedin.com/in/tinsayesimeneh/" target="_blank" rel="noreferrer">
               <img
                 src={linkedin}
                 alt="linkedin"
                 className="img-fluid my-3"
               />
+              </a>
             </div>
           </div>
         </div>
