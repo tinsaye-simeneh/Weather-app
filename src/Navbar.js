16d68395ca9690c961a9e5dc64dfe0ad5b157bad
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-15 13:27:52 +0300

    Navbar final for desktop

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,13 +5,29 @@ import linkedin from "./assets/img/linkedin.svg";
 
 const Navbar = () => {
   return (
-    <div className="Container">
-      <div className="row">
-        <div className="col-md-4">Windy Weather App</div>
-        <div className="col-md-4">
-            <img src={github} alt="github" />
-            <img src={behance} alt="github" />
-            <img src={linkedin} alt="github" />
+    <div className="Container-fluid bg-dark">
+      <div className="row ps-5">
+        <div className="col-md-8 ms-3 text-white pt-3 ps-5">Windy Weather App</div>
+        <div className="col-md-3 ms-5">
+          <div className="row ps-5">
+            <div className="col-md-3 text-center">
+              <img src={github} alt="github" className="img-fluid w-50 my-3" />
+            </div>
+            <div className="col-md-3 text-center">
+              <img
+                src={behance}
+                alt="behance"
+                className="img-fluid w-50 my-3"
+              />
+            </div>
+            <div className="col-md-3">
+              <img
+                src={linkedin}
+                alt="linkedin"
+                className="img-fluid w-50 my-3"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
